227350d670e360d780f0f64b948a4644bf28f7be
Nick Vissari <nvissari@hcpss.org>
2019-07-02 15:11:28 -0400

    Add backup procedure

--- backup.js
+++ backup.js
@@ -1,45 +1,33 @@
 'use strict'
 const fs = require('fs')
-const args = require('./get-args.js')
-
+const csv = require('fast-csv')
+const args = require('./get-args')
+const get_keys = require('./get-keys')
 
 const redis = require('redis'),
   rdb = redis.createClient('redis://' + args.server)
 
 if(args.action === 'backup'){
-
-}else if(args.action === 'restore'){
-
-}else{
-  console.log('Nothing to do')
-}
-
-
-// get all keys and data from prod
-rdb.keys('*', (err, keys) => {
-  const promises = []
-  for(const k in keys){
-    const key = keys[k]
-    const promise = new Promise((resolve, reject) => {
-      rdb.get(key, (err, data) => {
-        if(err) reject(err)
-        resolve({
-          key: key,
-          data: data
+  get_keys.then(keys => {
+    const promises = []
+    for(const i in keys){
+      const key = keys[i]
+      promises.push(new Promise((resolve, reject) => {
+        rdb.get(key, (err, value) => {
+          if(err) reject(err)
+          resolve({
+            key: key,
+            value: Buffer.from(value).toString('base64'),
+          })
         })
-      })
-    })
-    promises.push(promise)
-  }
-  // write all data to failover
-  Promise.all(promises).then(results => {
-    for(const k in results){
-      const result = results[k]
-      const key = result.key
-      const data = result.data
-      rdbfail.set(key, data)
+      }))
     }
-    rdb.quit()
-    rdbfail.quit()
+    Promise.all(promises).then(results => {
+      rdb.quit()
+      csv.write(results, { headers: true })
+        .pipe(fs.createWriteStream(args.file))
+    })
   })
-})
+}else if(args.action === 'restore'){
+  console.log('yup')
+}
